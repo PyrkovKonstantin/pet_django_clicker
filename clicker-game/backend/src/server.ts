7812dfaa9ce8c67ae36bca60b40e2d908cac33d3
PyrkovKonstantin <pirkov.constanitn@yandex.ru>
2026-01-30 13:07:18 +0300

    feat: inversifyJS

--- clicker-game/backend/src/server.ts
+++ clicker-game/backend/src/server.ts
@@ -1,16 +1,23 @@
 import 'reflect-metadata';
 import dotenv from 'dotenv';
-import app from './app';
+// import app from './app';
+import { container } from './container';
+import { InversifyExpressHttpAdapter } from '@inversifyjs/http-express';
+import express from 'express'; 
 
 // Load environment variables
 dotenv.config();
 
 const PORT = process.env['PORT'] || 3000;
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+const adapter: InversifyExpressHttpAdapter = new InversifyExpressHttpAdapter(
+  container,
+);
+
+const application: express.Application = await adapter.build();
+
+application.listen(3000);
 
 // Graceful shutdown
 process.on('SIGINT', async () => {
